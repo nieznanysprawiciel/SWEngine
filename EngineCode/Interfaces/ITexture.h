a1867033986a80bb68d29ee9d10a9b1d535fdef2
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2015-07-02 21:59:05 +0200

    Oddzielenie obiektów związanych z API graficznym od EngineCore - dodanie interfejsów

--- EngineCode/Interfaces/ITexture.h
+++ EngineCode/Interfaces/ITexture.h
@@ -6,10 +6,8 @@ class ITexture
 {
 private:
 protected:
-	std::wstring			m_fileName;									///<Nazwa pliku z tekstur¹.
 public:
-	std::wstring&			GetFileName() { return m_fileName; }		///<Zwraca nazwê pliku, który pos³u¿y³ do stworzenia obiektu.
-
 	virtual bool			CreateFromFile( const std::wstring& fileName ) = 0;
+	virtual bool			CreateFromMemmory() = 0;
 };
 
