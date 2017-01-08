38aa0bdfc5d3e98118fb65fd65231e5229054d01
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-01-08 13:18:31 +0100

    String discrete interpolation

--- EngineCode/Tests/PropertyAnimationTest/InterpolatorsTest.cpp
+++ EngineCode/Tests/PropertyAnimationTest/InterpolatorsTest.cpp
@@ -39,7 +39,8 @@ RTTR_REGISTRATION
 }
 
 
-
+// ================================ //
+//
 template< typename PropType >
 void			TestProperty		( TestInterpolators& animClass, const std::string& propertyPath )
 {
@@ -93,10 +94,62 @@ void			TestProperty		( TestInterpolators& animClass, const std::string& property
 
 		animator->Animate( &animClass, 5.0f );
 		CHECK( animClass.GetField< PropType >() == 0 );
+
+		animator->Animate( &animClass, 6.0f );
+		CHECK( animClass.GetField< PropType >() == 0 );
 	}
 
 }
 
+
+template< typename PropType, Size ListSize >
+void			TestPropertyDiscrete	( TestInterpolators& animClass, const std::string& propertyPath, PropType (&valuesList)[ ListSize ], TimeType (&timeList)[ ListSize ] )
+{
+	SECTION( propertyPath.c_str() )
+	{
+		Ptr< AnimationImpl< PropType, PropertyPath< PropType > > > animator = MakePtr< AnimationImpl< PropType, PropertyPath< PropType > > >( &animClass, propertyPath );
+
+		Size init = 0;
+		if( timeList[ init ] == 0.0f )
+		{
+			// Should be false !!
+			CHECK( !animator->AddKey( timeList[ init ], valuesList[ init ] ) );
+			init = 1;
+		}
+
+		for( ; init < ListSize; ++init )
+		{
+			INFO( "Adding keys, key time : " << timeList[ init ] );
+			CHECK( animator->AddKey( timeList[ init ], valuesList[ init ] ) );
+		}
+
+		REQUIRE( animator->GetKeySet().Keys.size() == ListSize );
+		REQUIRE( animator->GetKeySet().Interpolators.size() == ListSize - 1 );
+
+		for( Size i = 0; i < ListSize; ++i )
+		{
+			INFO( "Checking keys, key time: " << timeList[ i ] );
+			CHECK( animator->GetKey( timeList[ i ] )->Value == valuesList[ i ] );
+		}
+
+		int divisions = 10;
+
+		for( Size i = 0; i < ListSize - 1; ++i )
+		{
+			TimeType timeInterval = timeList[ i + 1 ] - timeList[ i ];
+								
+			for( Size j = 0; j < divisions; ++ j )
+			{
+				TimeType curTime = timeList[ i ] + j * timeInterval / divisions;
+				INFO( "Checking animation, time: " << curTime );
+
+				animator->Animate( &animClass, curTime );
+				CHECK( animClass.GetField< PropType >() == valuesList[ i ] );
+			}
+		}
+
+	}
+}
 
 
 
@@ -112,6 +165,13 @@ TEST_CASE( "Interpolators", "[Interpolators Types]" )
 	TestProperty< int16 >( AnimClass, "IntField16" );
 	TestProperty< uint8 >( AnimClass, "UIntField8" );
 	TestProperty< int8 >( AnimClass, "IntField8" );
+
+	{
+		std::string valuesArray[] = { "Value 1", "Something important", "Something not important", "Interesting message", "Not interesting message" };
+		TimeType timeArray[] ={ 0.0f, 3.0f, 5.0f, 6.1f, 11.0f };
+
+		TestPropertyDiscrete< std::string >( AnimClass, "StringField", valuesArray, timeArray );
+	}
 }
 
 
