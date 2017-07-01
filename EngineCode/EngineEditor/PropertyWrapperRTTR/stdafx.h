5b8106ea1fddcbb64dcff98c7394a8a116b764b8
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2017-07-01 23:47:31 +0200

    Fill precompiled header

--- EngineCode/EngineEditor/PropertyWrapperRTTR/stdafx.h
+++ EngineCode/EngineEditor/PropertyWrapperRTTR/stdafx.h
@@ -4,4 +4,13 @@
 
 #pragma once
 
+#include "EngineEditor/PropertyWrapperRTTR/Build/IncludeRTTR.h"
+#include "swCommonLib/Common/TypesDefinitions.h"
+
+
+#include "EngineEditor/PropertyWrapperRTTR/Build/BuildContext.h"
+#include "EngineEditor/PropertyWrapperRTTR/Properties/Base/PropertyWrapper.h"
+#include "EngineEditor/PropertyWrapperRTTR/Properties/Complex/HierarchicalPropertyWrapper.h"
+
+#include "swCommonLib/Common/EngineObject.h"
 
